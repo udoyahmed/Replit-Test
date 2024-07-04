12c7ca69d4840e1a32c4826f44057dc87b965797
udoyahmed <ahmedudoy767@gmail.com>
2024-07-05 03:19:18 +0600

    complete setup

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+app.get("/", (req,res) => {
+    res.render("index.ejs");
+});
+app.get("/about", (req,res) => {
+    res.render("about.ejs");
+})
+app.get("/contact", (req,res) => {
+    res.render("contact.ejs");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
